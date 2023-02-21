58c4a91995e3136ffa13443cf2b355be12505c06
Eric Zhou <43918217+ericzhou27@users.noreply.github.com>
2023-02-20 20:42:20 -0800

    Add firebase + router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div
-        onClick={() => {
-          window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        }}
-        className="container" />
+      <div className="container" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+    apiKey: "AIzaSyBKZoVId7Jqbny0frdhEI6hVEyPX_GWvu4",
+    authDomain: "stirfrai-2d1d2.firebaseapp.com",
+    projectId: "stirfrai-2d1d2",
+    storageBucket: "stirfrai-2d1d2.appspot.com",
+    messagingSenderId: "393505458106",
+    appId: "1:393505458106:web:999317d41937929e9abf83",
+    measurementId: "G-8YYF0391R3"
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
