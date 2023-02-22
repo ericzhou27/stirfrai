087486deca3730a770e752f0f180fbe284fe6d2e
Matthew Radovan <firedrake969@hotmail.com>
2023-02-21 22:08:23 -0800

    Add CORS

--- api/app/app.py
+++ api/app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 from helm_auth import autobalance, service
 
