162d88eb83c2caecc4ab337dea076e50fa34d090
Matthew Radovan <firedrake969@hotmail.com>
2023-03-07 18:08:55 -0800

    playing around with landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -4,16 +4,21 @@ import '../App.css';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
+import splashImg from '../imgs/splash.jpg'
+
 function Landing() {
     const currentUser = auth.currentUser
     console.log("CURRENT - ", currentUser)
 
     return (
-        <div className="App">
+        <div className="App flex-full-height">
             <div className="container">
-                <Typography variant="h1" style={{padding: 30, alignContent: 'center'}}>Never worry about what your next meal will be!</Typography>
-                <Typography variant="h4" style={{padding: 20}}>Powered by AI, receive delicious recipes based on your preferences and nutritional goals!</Typography>
-                <Button variant="contained"><a href="/home">Get Started</a></Button>
+                {/* <div className='splash-image' style={{backgroundImage: `url(${splashImg})`}} /> */}
+                {/* <Typography variant="h3" style={{padding: 30}}>Never worry about what your next meal will be!</Typography> */}
+                <Typography variant="h2" style={{padding: 20}}>stirfr.ai</Typography>
+                <Typography variant="h3" style={{padding: 20}}>an ai-powered meal planner</Typography>
+                <Typography variant="h7" style={{padding: 20, maxWidth: 800, textAlign: 'center'}}>Guided by AI, receive delicious meal plans and recipes based on your preferences and nutritional goals!  Maintain full control of your meal plan at your discretion with our human-in-the-loop design.</Typography>
+                <Button variant="contained" size='large' style={{marginTop: 50}}><a href="/home">Get Started</a></Button>
             </div>
         </div>
     )
