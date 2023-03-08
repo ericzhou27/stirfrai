63e7ac87d095a9eed17216eb4964f85a1777abbb
Matthew Radovan <firedrake969@hotmail.com>
2023-03-08 13:34:57 -0800

    landing page fonts

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -15,12 +15,12 @@ function Landing() {
             <div className="container">
                 {/* <div className='splash-image' style={{backgroundImage: `url(${splashImg})`}} /> */}
                 {/* <Typography variant="h3" style={{padding: 30}}>Never worry about what your next meal will be!</Typography> */}
-                <Typography variant="h2" style={{ padding: 20 }}>stirfr.ai</Typography>
-                <Typography variant="h3" style={{ padding: 20 }}>an ai-powered meal planner</Typography>
-                <Typography variant="h7" style={{ padding: 20, maxWidth: 800, textAlign: 'center' }}>Guided by AI, receive delicious meal plans and recipes based on your preferences and nutritional goals!  Maintain full control of your meal plan at your discretion with our human-in-the-loop design.</Typography>
-                <div className="button" onClick={() => {
+                <Typography variant="h2" style={{ padding: 20, fontStyle: 'italic', fontFamily: 'Playfair Display' }}>stirfr.ai</Typography>
+                <Typography variant="h3" style={{ padding: 20, fontFamily: 'Playfair Display' }}>an ai-powered meal planner</Typography>
+                <Typography variant="h7" style={{ padding: 20, maxWidth: 800, textAlign: 'center', fontFamily: 'Playfair Display' }}>Guided by AI, receive delicious meal plans and recipes based on your preferences and nutritional goals!  Maintain full control of your meal plan at your discretion with our human-in-the-loop design.</Typography>
+                <div className="button" style={{ width: 200 }} onClick={() => {
                     history.push(`/home`)
-                }}><p style={{ margin: 0 }}>Get Started</p></div>
+                }}><p style={{ margin: 0, fontFamily: 'Playfair Display', fontStyle: 'italic' }}>Get Started</p></div>
             </div>
         </div>
     )
