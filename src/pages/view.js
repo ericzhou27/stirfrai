7edb9e93d841470053e1fd29dd105419ec6ab01f
Matthew Radovan <firedrake969@hotmail.com>
2023-03-07 19:06:58 -0800

    idk where _reboot.scss is coming from

--- src/pages/view.js
+++ src/pages/view.js
@@ -203,8 +203,6 @@ function View() {
                                         <>
                                             {recipeStrings.map(line => <div>{line}</div>)}
                                             {ingredientStrings.map(line => <div>{line}</div>)}
-                                            {/* <div>{selectedMeal.recipe}</div> */}
-                                            {/* <div>{JSON.stringify(selectedMeal.ingredients)}</div> */}
                                         </>
                                     )
                                     :
